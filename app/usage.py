252074d92b21a159b0839314482eb35c1b481d8b
Jakub Hudak <jakub.j.hudak@gmail.com>
2025-01-23 15:23:48 +0000

    RED: test_message_usage

---
+++ app/usage.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+
+class MessageUsage(BaseModel):
+    ...
+
+
+class Usage(BaseModel):
+    ...
---
+++ tests/unit/test_usage.py
@@ -0,0 +1,19 @@
+import datetime
+from app.message import Message
+from app.usage import MessageUsage
+
+
+def test_message_usage():
+    message_usage = MessageUsage(message=Message(text="test text", timestamp=datetime.datetime(2024, 1, 1, 0, 0, 0), id=42))
+    assert message_usage.message_id == 42
+    assert message_usage.timestamp == datetime.datetime(2024, 1, 1, 0, 0, 0)
+    assert message_usage.report_name is None
+    assert message_usage.credits_used == 1
+
+
+def test_report_usage():
+    ...
+
+
+def test_usage():
+    ...
